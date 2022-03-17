859dd75a195525280f6ec2cff21242f74abed812
Kiana Safavi Baygi <Ksafavibaygi7391@conestogac.on.ca>
2022-03-17 12:53:24 -0400

    added bet value

--- BlackJack/Source.c
+++ BlackJack/Source.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 #include "Scroing.h"
-
+extern int initialAmountOfChip = 500;
 
 int main() {
 	bool result = true;
 	int numofwins = numOfWins(result, 2);
+	printf("Your balance: %d chips\n", initialAmountOfChip);
+	int bet=0;
+	printf("Please select the amount of chips you'd like to bet.\n");
+	scanf_s("%d", &bet);
+	if (bet > (initialAmountOfChip / 2)) {
 
-	int chips = 500;
-
-	//dfdf
+	}
  
 	return 0;
 }
