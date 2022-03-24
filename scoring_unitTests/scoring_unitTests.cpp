44ab2562b3df6920daff1e0df789cbefbb81a631
Kiana Safavi Baygi <Ksafavibaygi7391@conestogac.on.ca>
2022-03-24 12:52:02 -0400

    added unit test

---
+++ scoring_unitTests/scoring_unitTests.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+ #include "../BlackJack/Scroing.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace scoringunitTests
+{
+	TEST_CLASS(scoringunitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
