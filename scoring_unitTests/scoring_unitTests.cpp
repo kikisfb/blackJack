056952f6005a6ac175757a097a7e0e6322afda69
Kiana Safavi Baygi <Ksafavibaygi7391@conestogac.on.ca>
2022-03-31 11:15:18 -0400

    source.c added

--- scoring_unitTests/scoring_unitTests.cpp
+++ scoring_unitTests/scoring_unitTests.cpp
@@ -13,9 +13,70 @@ namespace scoringunitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(winStreakTest_winningCondition)
 		{
+			int a = 5;
+			a = winStreak(true, a);
+			int expected = 6;
+			Assert::AreEqual(a, expected);
 			
 		}
+		
+		TEST_METHOD(winStreakTest_LosingCondition)
+		{
+			int a = 5;
+			a = winStreak(false, a);
+			int expected = 0;
+			Assert::AreEqual(a, expected);
+
+		}
+		TEST_METHOD(LoseStreakTest_winningCondition)
+		{
+			int a = 5;
+			a = loseStreak(true, a);
+			int expected = 0;
+			Assert::AreEqual(a, expected);
+
+		}
+		TEST_METHOD(LoseStreakTest_losingCondition)
+		{
+			int a = 5;
+			a = loseStreak(false, a);
+			int expected = 6;
+			Assert::AreEqual(a, expected);
+		}
+		TEST_METHOD(numberOfLosesTest_winningCondition)
+		{
+			int a = 5;
+			a = numOfLoses(true, a);
+			int expected = 5;
+			Assert::AreEqual(a, expected);
+
+		}
+		TEST_METHOD(numberOfLosesTest_LosingCondition)
+		{
+			int a = 5;
+			a = numOfLoses(false, a);
+			int expected = 6;
+			Assert::AreEqual(a, expected);
+
+		}
+		TEST_METHOD(numberOfWinsTest_WinningCondition)
+		{
+			int a = 5;
+			a = numOfWins(true, a);
+			int expected = 6;
+			Assert::AreEqual(a, expected);
+
+		}
+		TEST_METHOD(numberOfWinsTest_LosingCondition)
+		{
+			int a = 5;
+			a = numOfWins(false, a);
+			int expected = 5;
+			Assert::AreEqual(a, expected);
+
+		}
+
 	};
 }
